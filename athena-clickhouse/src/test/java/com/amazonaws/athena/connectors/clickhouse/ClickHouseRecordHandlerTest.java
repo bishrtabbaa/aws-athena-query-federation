d40da28d3694e4fe7e87fcb6a00f2eefab656885
my-demo-cli-user-at-645411899653 <bishrt@amazon.com>
2025-06-12 07:05:13 -0700

    adding QPT support for Athena Clickhouse connector

---
+++ athena-clickhouse/src/test/java/com/amazonaws/athena/connectors/clickhouse/ClickHouseRecordHandlerTest.java
@@ -0,0 +1,225 @@
+/*-
+ * #%L
+ * athena-clickhouse
+ * %%
+ * Copyright (C) 2019 - 2025 Amazon Web Services
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.amazonaws.athena.connectors.clickhouse;
+
+import com.amazonaws.athena.connector.lambda.data.FieldBuilder;
+import com.amazonaws.athena.connector.lambda.data.SchemaBuilder;
+import com.amazonaws.athena.connector.lambda.domain.Split;
+import com.amazonaws.athena.connector.lambda.domain.TableName;
+import com.amazonaws.athena.connector.lambda.domain.predicate.Constraints;
+import com.amazonaws.athena.connector.lambda.domain.predicate.Marker;
+import com.amazonaws.athena.connector.lambda.domain.predicate.OrderByField;
+import com.amazonaws.athena.connector.lambda.domain.predicate.Range;
+import com.amazonaws.athena.connector.lambda.domain.predicate.SortedRangeSet;
+import com.amazonaws.athena.connector.lambda.domain.predicate.ValueSet;
+import com.amazonaws.athena.connector.lambda.domain.predicate.OrderByField.Direction;
+import com.amazonaws.athena.connectors.jdbc.connection.DatabaseConnectionConfig;
+import com.amazonaws.athena.connectors.jdbc.connection.JdbcConnectionFactory;
+import com.amazonaws.athena.connector.credentials.CredentialsProvider;
+import com.amazonaws.athena.connectors.jdbc.manager.JdbcSplitQueryBuilder;
+import com.amazonaws.athena.connectors.mysql.MySqlFederationExpressionParser;
+import com.amazonaws.athena.connectors.mysql.MySqlQueryStringBuilder;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.apache.arrow.vector.types.Types;
+import org.apache.arrow.vector.types.pojo.Schema;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import software.amazon.awssdk.services.athena.AthenaClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+
+import static com.amazonaws.athena.connectors.mysql.MySqlConstants.MYSQL_NAME;
+import static org.mockito.ArgumentMatchers.nullable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class ClickHouseRecordHandlerTest {
+    
+    private ClickHouseRecordHandler recordHandler;
+    private Connection connection;
+    private JdbcConnectionFactory jdbcConnectionFactory;
+    private JdbcSplitQueryBuilder jdbcSplitQueryBuilder;
+    private S3Client amazonS3;
+    private SecretsManagerClient secretsManager;
+    private AthenaClient athena;
+
+        @Before
+    public void setup()
+            throws Exception
+    {
+        this.amazonS3 = Mockito.mock(S3Client.class);
+        this.secretsManager = Mockito.mock(SecretsManagerClient.class);
+        this.athena = Mockito.mock(AthenaClient.class);
+        this.connection = Mockito.mock(Connection.class);
+        this.jdbcConnectionFactory = Mockito.mock(JdbcConnectionFactory.class);
+        Mockito.when(this.jdbcConnectionFactory.getConnection(nullable(CredentialsProvider.class))).thenReturn(this.connection);
+        jdbcSplitQueryBuilder = new MySqlQueryStringBuilder("`", new MySqlFederationExpressionParser("`"));
+        final DatabaseConnectionConfig databaseConnectionConfig = new DatabaseConnectionConfig("testCatalog", MYSQL_NAME,
+                "clickhouse://jdbc:clickhouse://hostname/user=A&password=B");
+
+        this.recordHandler = new ClickHouseRecordHandler(databaseConnectionConfig, amazonS3, secretsManager, athena, jdbcConnectionFactory, jdbcSplitQueryBuilder, com.google.common.collect.ImmutableMap.of());
+    }
+
+    @Test
+    public void buildSplitSql()
+            throws SQLException
+    {
+        TableName tableName = new TableName("testSchema", "testTable");
+
+        SchemaBuilder schemaBuilder = SchemaBuilder.newBuilder();
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol1", Types.MinorType.INT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol2", Types.MinorType.VARCHAR.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol3", Types.MinorType.BIGINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol4", Types.MinorType.FLOAT4.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol5", Types.MinorType.SMALLINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol6", Types.MinorType.TINYINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol7", Types.MinorType.FLOAT8.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol8", Types.MinorType.BIT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("partition_name", Types.MinorType.VARCHAR.getType()).build());
+        Schema schema = schemaBuilder.build();
+
+        Split split = Mockito.mock(Split.class);
+        Mockito.when(split.getProperties()).thenReturn(Collections.singletonMap("partition_name", "p0"));
+        Mockito.when(split.getProperty(Mockito.eq("partition_name"))).thenReturn("p0");
+
+        Range range1a = Mockito.mock(Range.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(range1a.isSingleValue()).thenReturn(true);
+        Mockito.when(range1a.getLow().getValue()).thenReturn(1);
+        Range range1b = Mockito.mock(Range.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(range1b.isSingleValue()).thenReturn(true);
+        Mockito.when(range1b.getLow().getValue()).thenReturn(2);
+        ValueSet valueSet1 = Mockito.mock(SortedRangeSet.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(valueSet1.getRanges().getOrderedRanges()).thenReturn(ImmutableList.of(range1a, range1b));
+
+        ValueSet valueSet2 = getRangeSet(Marker.Bound.EXACTLY, "1", Marker.Bound.BELOW, "10");
+        ValueSet valueSet3 = getRangeSet(Marker.Bound.ABOVE, 2L, Marker.Bound.EXACTLY, 20L);
+        ValueSet valueSet4 = getSingleValueSet(1.1F);
+        ValueSet valueSet5 = getSingleValueSet(1);
+        ValueSet valueSet6 = getSingleValueSet(0);
+        ValueSet valueSet7 = getSingleValueSet(1.2d);
+        ValueSet valueSet8 = getSingleValueSet(true);
+
+        Constraints constraints = new Constraints(
+            new ImmutableMap.Builder<String, ValueSet>()
+                .put("testCol1", valueSet1)
+                .put("testCol2", valueSet2)
+                .put("testCol3", valueSet3)
+                .put("testCol4", valueSet4)
+                .put("testCol5", valueSet5)
+                .put("testCol6", valueSet6)
+                .put("testCol7", valueSet7)
+                .put("testCol8", valueSet8)
+                .build(),
+            ImmutableList.of(),
+            ImmutableList.of(
+                new OrderByField("testCol1", Direction.ASC_NULLS_FIRST),
+                new OrderByField("testCol3", Direction.DESC_NULLS_FIRST)
+            ),
+            100L
+        );
+
+        String expectedSql = "SELECT `testCol1`, `testCol2`, `testCol3`, `testCol4`, `testCol5`, `testCol6`, `testCol7`, `testCol8` FROM `testSchema`.`testTable` PARTITION(p0)  WHERE (`testCol1` IN (?,?)) AND ((`testCol2` >= ? AND `testCol2` < ?)) AND ((`testCol3` > ? AND `testCol3` <= ?)) AND (`testCol4` = ?) AND (`testCol5` = ?) AND (`testCol6` = ?) AND (`testCol7` = ?) AND (`testCol8` = ?) ORDER BY `testCol1` ASC, ISNULL(`testCol3`) DESC, `testCol3` DESC LIMIT 100";
+        PreparedStatement expectedPreparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(Mockito.eq(expectedSql))).thenReturn(expectedPreparedStatement);
+
+        PreparedStatement preparedStatement = this.recordHandler.buildSplitSql(this.connection, "testCatalogName", tableName, schema, constraints, split);
+
+        Assert.assertEquals(expectedPreparedStatement, preparedStatement);
+    }
+
+
+    @Test
+    public void passthroughSql()
+            throws SQLException
+    {
+
+        String expectedSql = "SELECT * FROM TABLE(system.query(query => 'SELECT AVG(testCol1), testCol2 FROM testSchema.testTable WHERE 1=1 GROUP BY testCol2 ORDER BY testCol2 ASC LIMIT 100'))";
+
+        TableName tableName = new TableName("testSchema", "testTable");
+
+        SchemaBuilder schemaBuilder = SchemaBuilder.newBuilder();
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol1", Types.MinorType.INT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol2", Types.MinorType.VARCHAR.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol3", Types.MinorType.BIGINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol4", Types.MinorType.FLOAT4.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol5", Types.MinorType.SMALLINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol6", Types.MinorType.TINYINT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol7", Types.MinorType.FLOAT8.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("testCol8", Types.MinorType.BIT.getType()).build());
+        schemaBuilder.addField(FieldBuilder.newBuilder("partition_name", Types.MinorType.VARCHAR.getType()).build());
+        Schema schema = schemaBuilder.build();
+
+        HashMap<String, String> queryPassthroughArguments = new HashMap<>(3);
+
+        queryPassthroughArguments.put("QUERY", expectedSql);
+        queryPassthroughArguments.put("ARGUMENTS", "");
+        queryPassthroughArguments.put("schemaFunctionName", "system.query");
+        Split split = Mockito.mock(Split.class);
+
+        Constraints constraints = new Constraints(
+            Collections.emptyMap(),
+            Collections.emptyList(),
+            Collections.emptyList(),
+            100L,
+            queryPassthroughArguments
+        );
+
+        // SELECT * FROM TABLE(system.query(query => 'query string')
+        // SELECT * FROM TABLE(system.query(query => 'SELECT AVG(testCol1), testCol2 FROM testSchema.testTable WHERE 1=1 GROUP BY testCol2 ORDER BY testCol2 ASC LIMIT 100";'))
+        // "SELECT AVG(testCol1), testCol2 FROM testSchema.testTable WHERE 1=1 GROUP BY testCol2 ORDER BY testCol2 ASC LIMIT 100"
+
+        PreparedStatement expectedPreparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(Mockito.eq(expectedSql))).thenReturn(expectedPreparedStatement);
+
+        PreparedStatement preparedStatement = this.recordHandler.buildSplitSql(this.connection, "testCatalogName", tableName, schema, constraints, split);
+
+        Assert.assertEquals(expectedPreparedStatement, preparedStatement);
+    }
+
+        private ValueSet getSingleValueSet(Object value) {
+        Range range = Mockito.mock(Range.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(range.isSingleValue()).thenReturn(true);
+        Mockito.when(range.getLow().getValue()).thenReturn(value);
+        ValueSet valueSet = Mockito.mock(SortedRangeSet.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(valueSet.getRanges().getOrderedRanges()).thenReturn(Collections.singletonList(range));
+        return valueSet;
+    }
+
+    private ValueSet getRangeSet(Marker.Bound lowerBound, Object lowerValue, Marker.Bound upperBound, Object upperValue) {
+        Range range = Mockito.mock(Range.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(range.isSingleValue()).thenReturn(false);
+        Mockito.when(range.getLow().getBound()).thenReturn(lowerBound);
+        Mockito.when(range.getLow().getValue()).thenReturn(lowerValue);
+        Mockito.when(range.getHigh().getBound()).thenReturn(upperBound);
+        Mockito.when(range.getHigh().getValue()).thenReturn(upperValue);
+        ValueSet valueSet = Mockito.mock(SortedRangeSet.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(valueSet.getRanges().getOrderedRanges()).thenReturn(Collections.singletonList(range));
+        return valueSet;
+    }
+
+
+}
